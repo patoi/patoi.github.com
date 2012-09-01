8583b742a9312103bd360ca83c47d914860a5adb
István Pató <istvan.pato@gmail.com>
2012-09-01 11:38:54 +0200

    blogging

--- start_deploy_and_push.sh
+++ start_deploy_and_push.sh
@@ -7,4 +7,5 @@ git commit -a -m "blogging"
 git push origin source
 
 # push to developerof.com site
-wput ./_deploy/* ftp://patoi@developerof.com:tajkep1972@developerof.com
+wput --basename _deploy/ ./_deploy/* ftp://patoi@developerof.com:tajkep1972@developerof.com
+
