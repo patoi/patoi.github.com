b45f48c279ba3d48a8f5088634b1c83410ba80d6
István Pató <istvan.pato@gmail.com>
2012-09-14 16:00:22 +0200

    blogging

--- start_deploy_and_push.sh
+++ start_deploy_and_push.sh
@@ -7,5 +7,5 @@ git commit -a -m "blogging"
 git push origin source
 
 # push to developerof.com site
-// wrong: it doesn't overwite it... wput --basename _deploy/ ./_deploy/* ftp://patoi@developerof.com:tajkep1972@developerof.com
+# wrong: it doesn't overwite it... wput --basename _deploy/ ./_deploy/* ftp://patoi@developerof.com:tajkep1972@developerof.com
 
